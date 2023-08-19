f0f32b0429b1283224a759fc1fbf68ee56852e88
Raj Patil <63945913+rajpatil7322@users.noreply.github.com>
2023-08-19 12:10:48 +0530

    Update Swap.sol to make it more composable

--- Swap.sol
+++ Swap.sol
@@ -6,12 +6,10 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+//Swaprouter address 0xE592427A0AEce92De3Edee1F18E0157C05861564
+
 contract Swap {
     ISwapRouter public immutable swapRouter;
-
-    IERC20 public constant dai =IERC20(0xF14f9596430931E177469715c591513308244e8F);
-
-    IERC20 public constant weth =IERC20(0xD087ff96281dcf722AEa82aCA57E8545EA9e6C96);
 
     uint24 public constant poolFee = 3000;
 
@@ -20,21 +18,21 @@ contract Swap {
     }
 
         
-    function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
+    function swapExactInputSingle(address _tokenIn,address _tokenOut,uint256 amountIn) external returns (uint256 amountOut) {
         // msg.sender must approve this contract
 
         // Transfer the specified amount of DAI to this contract.
-        TransferHelper.safeTransferFrom(address(dai), msg.sender, address(this), amountIn);
+        TransferHelper.safeTransferFrom(_tokenIn, msg.sender, address(this), amountIn);
 
         // Approve the router to spend DAI.
-        TransferHelper.safeApprove(address(dai), address(swapRouter), amountIn);
+        TransferHelper.safeApprove(_tokenIn, address(swapRouter), amountIn);
 
                 // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
         // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
         ISwapRouter.ExactInputSingleParams memory params =
             ISwapRouter.ExactInputSingleParams({
-                tokenIn: address(dai),
-                tokenOut: address(weth),
+                tokenIn: _tokenIn,
+                tokenOut: _tokenOut,
                 fee: poolFee,
                 recipient: msg.sender,
                 deadline: block.timestamp,
